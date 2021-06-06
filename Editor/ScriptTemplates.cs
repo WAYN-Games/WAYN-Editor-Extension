e8560d15d6bf7eb3ed58509c2f05f03b81541459
WAYNGROUP <marc.hanicot@wayn-group.com>
2021-06-06 09:32:04 +0200

    Initial Commit

---
+++ Editor/ScriptTemplates.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace WaynGroup.Mgm.Ability
+{
+    internal class ScriptTemplates
+    {
+
+        public const string TemplatesRoot = "Packages/com.wayn_group.editor.extension/Editor";
+
+        [MenuItem("Assets/Create/WAYN/Editor/ExtendedScriptableObjectDrawer")]
+        public static void CreateAbilityEffectType()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"{TemplatesRoot}/ExtendedScriptableObjectDrawerTemplate.txt",
+                "NewEffect.cs");
+        }
+       
+    }
+}
